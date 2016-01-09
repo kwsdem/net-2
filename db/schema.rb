c053fab25c7a2f4657c3147dbcf55d9dc9ebed7a
Giannis Dem <giannidis50@hotmail.com>
2016-01-09 12:59:43 +0200

    added details to users\

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150626181431) do
+ActiveRecord::Schema.define(version: 20160109105759) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20150626181431) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "surname"
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

487926ddb73df26e0322ed5d30640eed1d3b0967
Giannis Dem <giannidis50@hotmail.com>
2016-01-16 21:12:16 +0200

    deleted mount

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,6 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  mount_uploader :picture
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validate  :picture_size
